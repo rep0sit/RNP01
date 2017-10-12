99ac34e58882f6ce3bba33ca33088bb29d3bf68d
Etienne Onasch <etienneonasch@web.de>
2017-10-12 15:00:26 +0200

    provisorische Kommentar in Interface hinzugefügt

--- src/main/MailFile.java
+++ src/main/MailFile.java
@@ -5,8 +5,21 @@ package main;
  *
  */
 public interface MailFile {
+	/**
+	 * Methode für einen login auf einem SMTP Server. <br>
+	 * 
+	 */
 	public void loginSMTP();
+	/**
+	 * Methode um sich vom SMTP Server abzumelden.<br>
+	 */
 	public void quitSMTP();
+	/**
+	 * Methode um eine Mail zu verschicken.<br>
+	 */
 	public void sendMail();
+	/**
+	 * Methode um alle relevanten Streams zu schließen.<br>
+	 */
 	public void closeFile();
 }
